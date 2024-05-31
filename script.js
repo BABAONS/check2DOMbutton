fa57b61d2909b404483bcedd91a9da5398e60dce
ONS BABA <nousababa72@gmail.com>
2024-05-31 19:34:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const body =document.querySelector('body');
+const button =document.querySelector('button');
+const colors=['violet','red', 'green','pink','black'];
+
+body.style.backgroundColor='violet';
+button.addEventListener('click',function(){
+    const colorIndex=parseInt(Math.random()*colors.length);
+    body.style.backgroundColor=colors[colorIndex];
+
+})
+
+
