f9be184b06204d8fe244fe0e5981b57746379e9e
ONS BABA <nousababa72@gmail.com>
2024-06-03 00:42:32 +0200

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+//déclaration des éléments html en code script
+
 const body =document.querySelector('body');
 const button =document.querySelector('button');
 const colors=['violet','red', 'green','pink','black'];
 
 body.style.backgroundColor='violet';
+//function pour changer la couleur de fond de la page en une couleur aléatoire parmi un tableau de couleurs prédéfini.
 button.addEventListener('click',function(){
     const colorIndex=parseInt(Math.random()*colors.length);
     body.style.backgroundColor=colors[colorIndex];
